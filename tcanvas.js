205f59f8dc931af67a8e2ef366217b3fe49f6541
tommaso.sottili@gmail.com <tommaso.sottili@gmail.com>
2017-07-26 22:04:42 +0200

    playing with inheritance

--- tcanvas.js
+++ tcanvas.js
@@ -1,45 +1,44 @@
-console.log("tcanvas.js");
-
-function TCanvas()
+function Scene()
 {
+
   this.canvas = 0;
   this.ctx = 0;
 
   this.bind = function(id)
   {
-    this.canvas = document.getElementById("myCanvas");
+    this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
   }
 
+
 }
 
+function TCanvas()
+{
+  Scene.call(this);
+
+  this.build = function()
+  {
+    var D = new Deck();
+    D.make(Card);
+    D.shuffle();
+    D.log();
+
+    var rows = D.seedsCount();
+    var cols = D.cardsPerSeed();
+
+    this.ctx.fillStyle = "#076324";
+    this.ctx.fillRect(0,0,this.canvas.width,this.canvas.height);
+
+    D.draw(this, 10, 10, rows, cols, 60, 100, 5, 5);
+
+  }
+}
 
 function draw()
 {
-  console.log("drawCanvas()");
-
-  var D = new Deck();
-  D.make(Card);
-  D.shuffle();
-  D.log();
-
-  console.log("done");
-
   var TCV = new TCanvas();
   TCV.bind("myCanvas");
-
-  var rows = D.seedsCount();
-  var cols = D.cardsPerSeed();
-
-  TCV.ctx.fillStyle = "#076324";
-  TCV.ctx.fillRect(0,0,TCV.canvas.width,TCV.canvas.height);
-
-  D.draw(TCV, 10, 10, rows, cols, 60, 100, 5, 5);
-
-/*
-  hexstring = "\u2660 \u2661 \u2662 \u2663 \u2664 \u2665 \u2666 \u2667 \u2668 \u2669";
-
-  ctx.fillStyle = "#000000"
-  ctx.fillText(hexstring,10,100);*/
+  TCV.build();
 
 }
